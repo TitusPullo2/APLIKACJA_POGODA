ccbbdf86db31c1a2631b7872a59189afabcf804d
TitusPullo2 <148714033+TitusPullo2@users.noreply.github.com>
2023-12-02 17:09:13 +0100

    grafika v.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,12 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.gms:play-services-location:17.1.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.squareup.picasso:2.71828")
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
